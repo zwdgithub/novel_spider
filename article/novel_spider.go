5d43f894de7eee352236d6c8be2ef88b86fc4c54
张卫东 <zhangweidong@xin.com>
2020-09-16 12:45:13 +0800

    0915

--- article/novel_spider.go
+++ article/novel_spider.go
@@ -177,7 +177,7 @@ func (s *NovelSpider) Process(obj NewArticle, c chan int) {
 		match = true
 	}
 	for _, item := range allChapters {
-		log.Infof(">%s, %s <", item.ChapterName, local.Lastchapter)
+		//log.Infof(">%s, %s<", item.ChapterName, local.Lastchapter)
 		if item.ChapterName == local.Lastchapter {
 			match = true
 			continue
@@ -193,7 +193,6 @@ func (s *NovelSpider) Process(obj NewArticle, c chan int) {
 			log.Infof("process %s, no chapter match, info: %s, %s, %s, %s", obj.Url, local.Articlename, local.Author, allChapters[len(allChapters)-1].ChapterName, local.Lastchapter)
 		}
 		for _, item := range allChapters {
-			log.Infof(">%s, %s<", item.ChapterName, second)
 			if item.ChapterName == second {
 				match = true
 				log.Infof("process %s, try to match last second chapter success", obj.Url)
