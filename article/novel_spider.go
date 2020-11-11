750b7dc2168dad631e786abaac5b535fed5f1cdb
zwdgithub <zwdjobs@126.com>
2020-11-11 10:59:41 +0800

    1009

--- article/novel_spider.go
+++ article/novel_spider.go
@@ -262,6 +262,7 @@ func (s *NovelSpider) Process(obj NewArticle, c chan int) {
 			}
 			for _, c := range localCache {
 				score := strsim.Compare(content, c, strsim.DiceCoefficient())
+				log.Infof("process %s, try to match score: %d", obj.Url, score)
 				if score >= 0.75 {
 					match = true
 					for j := i + 1; j < len(allChapters); j++ {
